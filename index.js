37165495a370c578652e6506e9dda3aa3a38a0f4
Bisiedigital <bisie777@gmail.com>
2024-07-02 06:39:29 +0300

    Added public defaultProps in index.js

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
  * @format
  */
 
+import { Text } from 'react-native';
+
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.style = { color: 'black' }; // Set your desired color
+
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
